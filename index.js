cb81489776484e7fbab11fa7dc1f07b9a7a926b1
Marylucy <marylucyatienoomenda.gmail.com>
2022-11-25 17:21:37 -0500

    completed assignment

---
+++ index.js
@@ -0,0 +1,19 @@
+function receivesAFunction(callback) {
+    return callback();
+}
+
+receivesAFunction(() =>{
+    
+});
+
+function returnsANamedFunction() {
+    let sam = () => console.log("mans")
+    return (sam)
+}
+
+var returnsAnAnonymousFunction = function() {
+    return (() => {
+    alert('This an anonymous function')
+    }
+    )
+}
